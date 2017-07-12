434c96ad91516f03bc79ad6b35130d119d7d20c5
Isak La Fleur <isak.engdahl@gmail.com>
2017-07-12 20:44:45 +0200

    solved first challange

---
+++ solutions.js
@@ -0,0 +1,8 @@
+// Looping a triangle
+let triangle = '';
+for (let i = 0; i < 7; i++) {
+  triangle = triangle + '#';
+  console.log(triangle);
+};
+// ====================================== //
+// FizzBuzz
